0aeda55df75c9ba9cf64191947965446c47221c2
Marcelo Macedo <marcelo3macedo@gmail.com>
2022-08-27 09:38:38 -0300

    feat: Two Database connections

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,13 +1,27 @@
-module.exports = {
-  "type": process.env.TYPEORM_TYPE,
-  "host": process.env.TYPEORM_HOST,
-  "port": process.env.TYPEORM_PORT,
-  "username": process.env.TYPEORM_USERNAME,
-  "password": process.env.TYPEORM_PASSWORD,
-  "database": process.env.TYPEORM_DATABASE,
-  "entities": [ process.env.TYPEORM_ENTITIES ],
-  "migrations": [ process.env.TYPEORM_MIGRATIONS ],
-  "cli": {
-    "migrationsDir":  process.env.TYPEORM_CLI_MIGRATIONSDIR
+module.exports = [
+  {
+    "name": process.env.NOTIFIER_NAME,
+    "type": process.env.NOTIFIER_TYPE,
+    "default": true,
+    "host": process.env.NOTIFIER_HOST,
+    "port": process.env.NOTIFIER_PORT,
+    "username": process.env.NOTIFIER_USERNAME,
+    "password": process.env.NOTIFIER_PASSWORD,
+    "database": process.env.NOTIFIER_DATABASE,
+    "entities": [ process.env.NOTIFIER_ENTITIES ],
+    "migrations": [ process.env.NOTIFIER_MIGRATIONS ],
+    "cli": {
+      "migrationsDir":  process.env.NOTIFIER_CLI_MIGRATIONSDIR
+    }
+  },
+  {
+    "name": process.env.API_NAME,
+    "type": process.env.API_TYPE,
+    "host": process.env.API_HOST,
+    "port": process.env.API_PORT,
+    "username": process.env.API_USERNAME,
+    "password": process.env.API_PASSWORD,
+    "database": process.env.API_DATABASE,
+    "entities": [ process.env.API_ENTITIES ],
   }
-}
+]
