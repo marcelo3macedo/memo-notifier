b41128c13e5241faaac6ff87576ed0fbb0e42163
Marcelo <marcelo3macedo@gmail.com>
2022-10-20 16:04:18 -0300

    feat: Variables Enviroment

--- src/lib/VariableManager.ts
+++ src/lib/VariableManager.ts
@@ -1,9 +1,9 @@
 import dotenv from 'dotenv'
 import path from 'path'
-import { ENVIROMENT_DEV } from '@constants/enviroments'
+import { ENVIROMENT_PROD } from '@constants/enviroments'
 
-const enviroment = process.env.npm_lifecycle_event === ENVIROMENT_DEV ? 
-    '' : 'production'
+const enviroment = process.env.npm_lifecycle_event === ENVIROMENT_PROD ? 
+    'production' : ''
     
 dotenv.config({
     path: path.resolve(__dirname, "../../", `${enviroment}.env`),
